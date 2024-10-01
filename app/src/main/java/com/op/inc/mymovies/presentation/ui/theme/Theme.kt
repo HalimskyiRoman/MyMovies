3c076f75159a2184a50ed47fa7d1f75a70bc777f
RomanHalimskyiJadex <roman.halimskyi@jadexconsulting.com>
2024-10-01 04:43:20 +0300

    added structural packages

--- app/src/main/java/com/op/inc/mymovies/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/op/inc/mymovies/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.op.inc.mymovies.ui.theme
+package com.op.inc.mymovies.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/op/inc/mymovies/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/op/inc/mymovies/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.op.inc.mymovies.ui.theme
+package com.op.inc.mymovies.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
